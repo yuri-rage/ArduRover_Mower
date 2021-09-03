f6dcd378d4a03a21a8bb2a9eae7f5b786e2c8e01
yuri-rage <yuri-rage@users.noreply.github.com>
2021-09-03 10:38:48 -0500

    added license

--- MinFixType.lua
+++ MinFixType.lua
@@ -1,12 +1,32 @@
-------------------------------------------------
------ MinFixType ArduPilot Lua script ------
---                                            --
--- Checks for mission running and commands    --
--- a hold/pause if the GPS fix type is less   --
--- than a threshold value.                    --
---                                            --
---------------- Yuri -- Aug 2021 ---------------
-------------------------------------------------
+--[[----------------------------------------------------------------------------
+
+MinFixType ArduPilot Lua script
+
+Checks for mission running and commands a hold/pause if the GPS fix type is less
+than a threshold value.
+
+CAUTION: This script is capable of engaging and disengaging autonomous control
+of a vehicle.  Use this script AT YOUR OWN RISK.
+
+-- Yuri -- Aug 2021
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------]]
 
 -------- MODE "CONSTANTS"   --------
 local ROVER_MODE_MANUAL    =  0
